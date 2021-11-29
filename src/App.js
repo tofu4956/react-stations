dba773a587ac11c440001a8ef7fb1e9ab3359acc
tofu4956 <tofu4956@users.noreply.github.com>
2021-11-29 20:54:17 +0900

    try station3

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css'
   */
   export const App = () => {
 
-    const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
+    const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
     return (
       <header>
         <h1>Dogアプリ</h1>
