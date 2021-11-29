d06d51aa1feadaa4da2c8b4b3595ababe157f117
tofu4956 <tofu4956@users.noreply.github.com>
2021-11-29 20:53:45 +0900

    try station3

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import './App.css'
   * @type {React.FC}
   */
   export const App = () => {
+
+    const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
     return (
       <header>
         <h1>Dogアプリ</h1>
         <p>犬の画像を表示します</p>
-        <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"></img>
+        <img src={dogUrl}></img>
       </header>
     )
   }
