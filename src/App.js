30100a06abc4bf7ee0bf2ea9a47692addd874916
tofu4956 <tofu4956@users.noreply.github.com>
2021-11-29 21:03:52 +0900

    try station4

--- src/App.js
+++ src/App.js
@@ -10,12 +10,17 @@ import './App.css'
   export const App = () => {
 
     const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
+    const randomDoge = async () => {
+      await fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => setDogUrl(data.message))
+    }
     return (
       <header>
         <h1>Dogアプリ</h1>
         <p>犬の画像を表示します</p>
         <img src={dogUrl}></img>
-        <button onClick={() => setDogUrl("images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")}>更新</button>
+        <button onClick={randomDoge}>更新</button>
       </header>
     )
   }
